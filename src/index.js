5014b531ac7c40003e1f02a1003f5f94e4fae035
Guseff <vasily.guseff@gmail.com>
2017-06-04 18:53:27 +0300

    router added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './containers/App';
+import { render } from 'react-dom';
+// import { createStore } from 'redux';
+// import { Provider } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+
+import App from './containers/App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(
+  <Router>
+    <Route exact path="/" component={App}>
+      {/*<IndexRoute component={Home} />
+      <Route path="article/:id" component={Article} />
+      <Route path="about" component={About} />
+      <Route path="login" component={Login} />*/}
+    </Route>
+  </Router>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
