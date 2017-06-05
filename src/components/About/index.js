cc75c652843748e7d981bc18f6f7525dbfa9cf3b
Guseff <vasily.guseff@gmail.com>
2017-06-05 22:29:34 +0300

    about css

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
+import './style.css';
+
 export default class About extends Component {
   render() {
     return (
       <div className="about">
-        This app made as a test service offer on map by Vasily. <br />
-        For lols only :)
+        <p>This app made as a test service offer on map by Vasily. <br />
+        For lols only :)</p>
       </div>
     );
   }
