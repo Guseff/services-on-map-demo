84fd5601c61e50c92a8fa64dc9497ae84b67b17e
Guseff <vasily.guseff@gmail.com>
2017-06-11 13:04:54 +0300

    location

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,6 +1,7 @@
-import React, { PropTypes, Component } from 'react';
+import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import LMap from '../../containers/LMap';
 import '../../assets/App.css';
