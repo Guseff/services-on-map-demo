31681aefa5842c6456b67ede920708649099053f
Guseff <vasily.guseff@gmail.com>
2017-06-26 12:46:13 +0300

    google auth attempt

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import GoogleLogin from 'react-google-login';
 
 import './style.css';
 
 class Header extends Component {
   render() {
+    const responseGoogle = (response) => {
+      console.log(response);
+    }
     return (
       <div className="head">
         <div className="logo">
@@ -14,6 +18,14 @@ class Header extends Component {
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
+            <li>
+              <GoogleLogin
+                clientId="732274642447-e5u8prip22va80rc4rou5tus04s3ecak.apps.googleusercontent.com"
+                buttonText="Login"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+              />
+            </li>
           </ul>
         </div>
       </div>
